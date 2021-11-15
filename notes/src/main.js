1f412d241af809446f1638e11d23caed827d05bf
范锦鸿 <1308228200@qq.com>
2021-11-15 10:39:05 +0800

    基础架构搭建

---
+++ notes/src/main.js
@@ -0,0 +1,45 @@
+import Vue from 'vue'
+import router from './router.js'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import App from './App.vue'
+import {Message} from 'element-ui'
+
+axios.defaults.baseURL = '/api';
+axios.defaults.timeout = 8000;
+//axios.defaults.baseURL = env.baseURL;
+
+
+
+// // 接口错误拦截
+// axios.interceptors.response.use(function(response) {
+//   let res = response.data;
+//   let path = location.hash;
+//   if(res.status == 0) {
+//     return res.data;
+//   } else if(res.status == 10) {
+//     if(path != '#/index') {
+//       window.location.href = '/#/login';
+     
+//     }
+//     return Promise.reject(res);
+//   } else {
+//     Message.warning(res.msg);
+//     return Promise.reject(res);
+//   }
+// },(error)=>{
+//   let res = error.response;
+//   Message.error(res.data.msg);
+//   return Promise.reject(error);
+// });
+// Vue.use(Message);
+Vue.use(VueAxios, axios);
+
+
+Vue.config.productionTip = false
+
+Vue.prototype.$message = Message;
+new Vue({
+  router,
+  render: h => h(App),
+}).$mount('#app')
---
+++ notes/src/pages/login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="login">
+   
+      11
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'Login',
+ 
+}
+</script>
+
+<style>
+
+</style>
